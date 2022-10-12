23e164074821e25929536d11679e81ca47074ad5
magician11 <support@andrewgolightly.com>
2022-10-12 12:24:53 +0100

    updated meta tag information

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -28,8 +28,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title="An elite web development company"
+      description="Professional web and app development"
     >
       <HomepageHeader />
       <main>
